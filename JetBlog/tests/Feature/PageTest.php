efc13ff7bf4093e1cb92788374d85438c3528d67
Eden <Voortmanraymond@gmail.com>
2023-11-01 11:22:50 +0100

    added unit tests for Auth and page existence

---
+++ JetBlog/tests/Feature/PageTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PageTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_home_page_exists(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_login_page_exists(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_register_page_exists(): void
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+}
